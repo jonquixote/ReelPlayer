65b82c8a1587dbd2b0aa034b401c59dcd9856ab4
Bo Jacobson <bjacobson26@gmail.com>
2014-12-12 21:24:45 -0800

    Mixer!!!!!

---
+++ MixerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MixerViewController.swift
+//  ReelPlayer
+//
+//  Created by Bo Jacobson on 12/12/14.
+//  Copyright (c) 2014 RealtoReal. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import AVFoundation
+
+let reuseIdentifier = "recordingCell"
+
+class MixerViewController: UIViewController {
+    
+}
