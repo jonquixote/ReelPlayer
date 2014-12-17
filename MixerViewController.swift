c84724353b72b8f132c12539ff13b21bd601a3b6
Bo Jacobson <bjacobson26@gmail.com>
2014-12-16 16:56:43 -0800

    nothing good

--- MixerViewController.swift
+++ MixerViewController.swift
@@ -12,4 +12,34 @@ import AVFoundation
 
 class MixerViewController: UIViewController {
     
+    
+    
+    
+@IBOutlet var track1slider: UISlider!
+
+    override func viewDidLoad() {
+        configuretrack1slider()
+        
+       
+
+    }
+    
+
+
+    
+func configuretrack1slider() {
+    
+    let thumbImage = UIImage(named: "faderknob1")
+    
+    track1slider.setThumbImage(thumbImage, forState: .Normal)
+
+    track1slider.addTarget(self, action: "sliderValueDidChange:", forControlEvents: .ValueChanged)
+    
+    track1slider = UISlider(frame: CGRect(x: 0, y: 0, width: 200, height: 23))
+    
+    track1slider.transform = CGAffineTransformMakeRotation(CGFloat(M_PI)/180.0)
+}
+
+    
+
 }
